af76c378f6dd5e994aacdc62524b7d352e863204
blasphemite <kevin@kevtx.com>
2022-08-15 20:25:58 -0500

    local.yml

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 if [[ $(id -u) -ne 0 ]]; then echo "Please run as root!" && exit 1 ; fi
 
 git_url="https://github.com/kevtx/ansible-pull"
-git_branch="dev_domain"
+git_branch="domain"
 vault_pw_file="/root/.ansible/.secrets/domain-join"
 
 if ! test -f "$vault_pw_file" ; then
@@ -16,11 +16,5 @@ echo -e "git_branch:\t$git_branch"
 echo -e "vault_pw_file:\t$vault_pw_file"
 
 cmd="/usr/bin/ansible-pull --url $git_url --checkout $git_branch --vault-password-file $vault_pw_file"
-
-#ansible-pull \
-#  #--only-if-changed \
-#  --vault-password-file "$vault_pw_file" \
-#  --checkout "$git_branch" \
-#  --url "$git_url"
 
 $cmd
